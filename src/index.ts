549c45a383bb0743e9f96675804a0fa07b517cb8
ahd3r <ander11110@gmail.com>
2022-06-13 13:29:17 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@ import { time } from './utils'
 
 console.log(moment().format('DD-MM-YYYY HH-mm-ss DD'));
 
-export const handler = (...args: any[]) => {
-  console.log(args);
-  return {
+export const handler = (event, context, callback) => {
+  console.log(event, context, callback);
+  callback(null, {
       statusCode: 200,
       body: JSON.stringify({data: time}),
-  };
+  });
 };
