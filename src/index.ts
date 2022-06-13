2224ed2e039a57603f69bf93897a41ed7e5e11d4
ahd3r <ander11110@gmail.com>
2022-06-13 14:00:22 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -102,7 +102,7 @@ interface ContextRequestI {
 }
 
 export const handler = (event: EventRequestI, context: ContextRequestI, callback: Function) => {
-  const data = 3;
+  const data = 4;
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({ data })
